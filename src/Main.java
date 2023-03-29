ae26c77c4340a315ea868d9108a84002b4fe6ddc
nima321 <niman8900@gmail.com>
2023-03-29 17:14:24 +0100

    i added the features but there are errors that need to be reesolved

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,11 @@ public class Main {
         solar.moon="2";
         solar.planets="2";
 
+        featureSun Xron=new featureSun();
+        Xron.temperature="55151651515";
+        Xron.radius=51151651651651;
+        Xron.color="pink";
+        
+
     }
 }
